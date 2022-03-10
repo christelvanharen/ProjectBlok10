829a051b7a21c8817d23530e46eda1bb0f7ebac6
christelvanharen <cca.vanharen@student.han.nl>
2022-03-10 11:25:39 +0100

    juiste bestand toegevoegd

--- read Cel file.py
+++ read Cel file.py
@@ -1,6 +1,6 @@
 from Bio.Affy import CelFile
 
-with open("GSM1019138_PA025_human_6A_070228.CEL", "rb") as handle:
+with open("GSM387788.CEL", "rb") as handle:
     c = CelFile.read(handle)
 # print(c.ncols, c.nrows)
 # print(c.intensities)
